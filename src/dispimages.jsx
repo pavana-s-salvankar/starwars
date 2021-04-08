51a4aedffcc9c430983559edc4540a507aad8e32
pavana-s-salvankar <pavanasalvankarbelle1999@gmail.com>
2021-04-08 15:12:15 +0530

    recat-practice-code

--- src/dispimages.jsx
+++ src/dispimages.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import {
     Link,
   } from "react-router-dom";
@@ -105,4 +105,83 @@ class  DispImages extends Component
        </div> );
     }
     }
+export default DispImages;
+*/
+import React, { Component } from 'react';
+import {Link, } from "react-router-dom";
+import logo from './images/logo.png';
+import people from './images/people_normal.png';
+import starship from './images/starship_normal.png';
+import films from './images/films_normal.png';
+import planets from './images/planets_normal.png';
+import species from './images/species_normal.png';
+import vehicles from './images/vehicles_normal.png';
+import footerLogo from './images/footer_logo.png';
+import './App.css';
+class DispImages extends Component {
+    state = {}
+    render() { 
+        const imgNames= [
+            {src:films,name:'FILMS'},
+            {src:species,name:'SPECIES'},
+            {src:planets,name:'PLANETS'},
+            {src:people,name:'PEOPLE'},
+            {src:starship,name:'STARSHIP'},
+            {src:vehicles,name:'VEHICLES'}];
+
+            const footerElements=[
+            {to:'/termsofuse' ,name:'TERMS OF USE'},
+            {to:'/legalnotices', name:'LEGAL NOTICES'},
+            {to:'/privacypolicy' ,name:'PRIVACY POLICY'},
+            {to:'/help',name:'STAR WARS HELPDESK'},
+            {to:'tdisneystore',name:'STAR WARS AI DISNEY STORE'}
+        ]
+
+        const images = imgNames.map(image => {
+            return (
+                     <li className='images' >
+                      <Link to ={`/${image.name.toLowerCase()}`}>
+                      <img key={image} src={image.src} />
+                          <div className='image-name'>{image.name}</div>
+                      </Link>
+                      </li>
+                      );
+            }
+        );
+        const footer = footerElements.map(ele => {
+            return (
+                <li className='footer-element'>
+                <Link to ={ele.to}>
+                    <div className='image-name'>{ele.name}</div>
+                 </Link>
+            </li>
+                      );
+            }
+        );
+        return ( 
+            <div >
+                <div className='logo'>
+                      <img src={logo} alt=''/>
+                </div>
+                <hr></hr>
+        <div className='images'>
+        <ul>
+            {images}
+       </ul>
+       <hr></hr>
+       </div>
+       <div className='images'>
+        <ul>
+        <li>
+                <img src={footerLogo} alt=''/ >
+                </li>
+            {footer}
+       </ul>
+       </div>
+       <div >
+        <p3>TM &copy; Lucasfilm Ltd. All Rights Reserved</p3>
+       </div> )
+    </div>);
+    }
+}
 export default DispImages;
